b9199272805e530c7f4a6a6463977684c65d47cf
Shashank Tiwari <shashanktiwari1312@gmail.com>
2024-03-10 21:27:25 +0530

    created a basic navbar component

--- frontend_react/src/components/Navbar/Navbar.tsx
+++ frontend_react/src/components/Navbar/Navbar.tsx
@@ -1,8 +1,21 @@
+import { images } from '../../constants';
 import './Navbar.scss';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav>
+      <div>
+        <img src={images.logo} alt="logo"/>
+      </div>
+      <ul>
+        {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
+          <li key={`link-${item}`}>
+            <div />
+            <a href={`#${item}`}>{item}</a>
+          </li>
+        ))}
+      </ul>
+    </nav>
   );
 }
 
