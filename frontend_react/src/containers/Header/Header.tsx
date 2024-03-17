2f78f6e6bf1fc8114ba7e064f4c35d62714b1087
Shashank Tiwari <shashanktiwari1312@gmail.com>
2024-03-17 12:18:18 +0530

    completed the header section

--- frontend_react/src/containers/Header/Header.tsx
+++ frontend_react/src/containers/Header/Header.tsx
@@ -45,7 +45,7 @@ const Header = () => {
         <img src={images.profile} alt="profile_bg" />
         <motion.img 
           whileInView={{ scale: [0, 1] }}
-          transition={{ duration: 1.5, ease: 'easeInOut' }}
+          transition={{ duration: 1, ease: 'easeInOut' }}
           src={images.circle}
           alt="profile_circle"
           className="overlay_circle"
