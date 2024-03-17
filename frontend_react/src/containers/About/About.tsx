9d036e8a7f45806c894602f827cb49e64460df2a
Shashank Tiwari <shashanktiwari1312@gmail.com>
2024-03-17 15:14:05 +0530

    added my logo

--- frontend_react/src/containers/About/About.tsx
+++ frontend_react/src/containers/About/About.tsx
@@ -1,8 +1,19 @@
+import { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
+
+import { images } from '../../constants';
 import './About.scss';
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+      <h2 className="head-text">
+        I Know That
+        <span>Good Design</span>
+        means
+        <span>Good Business</span>
+      </h2>
+    </>
   );
 }
 
