d2575d8b55925bd7d0aa1aba2e5e203f6f6236fb
Shashank Tiwari <shashanktiwari1312@gmail.com>
2024-03-17 16:13:07 +0530

    made some changes in logo size

--- frontend_react/src/containers/About/About.tsx
+++ frontend_react/src/containers/About/About.tsx
@@ -4,15 +4,24 @@ import { motion } from 'framer-motion';
 import { images } from '../../constants';
 import './About.scss';
 
+const abouts = [
+  {title: 'Web Development', description: ''}
+]
+
 const About = () => {
   return (
     <>
       <h2 className="head-text">
-        I Know That
+        I Know That 
         <span>Good Design</span>
+        <br />
         means
         <span>Good Business</span>
       </h2>
+
+      <div className="app__profiles">
+
+      </div>
     </>
   );
 }
