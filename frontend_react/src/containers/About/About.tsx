49a55d7863ed0e77967496f4c8f1c580a5fddd39
Shashank Tiwari <shashanktiwari1312@gmail.com>
2024-03-20 20:56:13 +0530

    modified about component

--- frontend_react/src/containers/About/About.tsx
+++ frontend_react/src/containers/About/About.tsx
@@ -24,7 +24,7 @@ const abouts = [
 const About = () => {
   return (
     <>
-      <h2 className="head-text">I Know That <span>Good Design</span><br />means <span>Good Business</span></h2>
+      <h2 className="head-text">I Know That <span>Good Development</span><br />means <span>Good Business</span></h2>
 
       <div className="app__profiles">
         {abouts.map((about, index) => (
